3a31ed931e0803d872ff083d5803d45de3a1c5b8
Akash Kumar <akashkumar979868@gmail.com>
2026-02-18 17:32:50 +0000

    done

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -37,3 +37,27 @@ const myFunction = function(){
 myFunction();
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// Stack(primitive) and Heap(Non - Primitive)
+let a = "akash";
+let b = a;
+b = "Kumar";
+console.log(a);
+console.log(b);
+// so when we use primitive datatypes then the values are stored in stack and when we cahnge or modify the original value then it gives a copy to us and changes are done in the copy variable not in the original variable.
+
+// Heap(Non primitive)
+let user1 = {
+    email:"abc12@gmail.com",
+    upi:"user@ybl"
+
+}
+
+let user2 = user1;
+user2.email = "night12@gmail.com";
+
+console.log(user1.email);
+console.log(user2.email);
+
+// so when we use non-primitive dataypes the data are stored in heap and when we modify the original variable by copying its variable into other variable then the original value also gets change or modify so we actually modify the original declared value.
+// isme original ki copy nhi milti, hume variable ki memory ki reference milti hai aur hume iske karn changes original mai dikhti hai.
